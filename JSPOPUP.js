208e1e62c831ca496da0f13ed0ae2395c62b1688
Ufuk Bakan <println.ufukbakan@gmail.com>
2019-02-10 19:23:22 +0300

    test

--- JSPOPUP.js
+++ JSPOPUP.js
@@ -1,16 +1,3 @@
-$(document).ready(function(){
-	$(document).on("click",".more",function(){
-		var id=$(this).attr("id");
-		$(".more").hide();
-		$(".moreloading").show();
-		$.ajax({
-			type:'POST',
-			url:'ajax.php',
-			data:'id='+id,
-			success:function(html){
-				$("#menu_"+id).remove();
-				$('#article_container').append(html);
-			}
-		});
-	});
-});
+function kayit(){
+	window.open("popup.html","mypop","directories=no,scrollbar=no,top=20,left=440,statusbar=1,toolbar=0,width=440px,height=623px");
+	}
--- popup_functions.js
+++ popup_functions.js
@@ -1,8 +1,8 @@
+
 	function tabcheck()
 	{
 	if(document.getElementById('tab1radio').checked)
 	{
-		
 		document.getElementById('current_tab').innerHTML = "Kayıt Ol";
 		document.getElementById('tab1').classList.add("active");
 		document.getElementById('tab2').classList.remove("active");
@@ -11,9 +11,16 @@
 		document.getElementById('tab2_content').style.visibility = "hidden";
 		document.getElementById('tab3_content').style.visibility = "hidden";		
 		document.getElementById("popup_window").style.height="750px";
-		document.getElementById("hatamesaji").style.visibility="visible";
-		document.getElementById("yanliskullanici").style.visibility="hidden";
-		
+	}
+	else if(document.getElementById('tab2radio').checked){
+		document.getElementById('current_tab').innerHTML = "E-posta Onayla";
+		document.getElementById('tab2').classList.add("active");
+		document.getElementById('tab1').classList.remove("active");
+		document.getElementById('tab3').classList.remove("active");
+		document.getElementById('tab2_content').style.visibility = "visible";
+		document.getElementById('tab1_content').style.visibility = "hidden";
+		document.getElementById('tab3_content').style.visibility = "hidden";		
+		document.getElementById("popup_window").style.height="300px";
 	}
 	else if(document.getElementById('tab3radio').checked){
 		document.getElementById('current_tab').innerHTML = "Giriş Yap";
@@ -23,30 +30,29 @@
 		document.getElementById('tab3_content').style.visibility = "visible";
 		document.getElementById('tab2_content').style.visibility = "hidden";
 		document.getElementById('tab1_content').style.visibility = "hidden";		
-		document.getElementById("popup_window").style.height="420px";		
-		document.getElementById("hatamesaji").style.visibility="hidden";
-		
-		document.getElementById("yanliskullanici").style.visibility="visible";
+		document.getElementById("popup_window").style.height="400px";
 	}
 	}
+
 	function tab_signup()
 	{
 		document.getElementById('tab1radio').click();
 
-	}	
+	}
+	
 	function tab_signin()
 	{
 		document.getElementById('tab3radio').click();
 
 	}
+
 	function cleardivs()
 	{
-		
 		document.getElementById('tab3_content').style.visibility = "hidden";
 		document.getElementById('tab2_content').style.visibility = "hidden";
 		document.getElementById('tab1_content').style.visibility = "hidden";
 		document.getElementById('tab1radio').checked = false;
 		document.getElementById('tab2radio').checked = false;
 		document.getElementById('tab3radio').checked = false;
-		
+
 	}
